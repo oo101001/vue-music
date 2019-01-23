eb4689a59b0edc86da3aba7ed1e16bf0f3fa2342
dinggang <dinggang@lvtudiangdian.com>
2019-01-23 18:08:44 +0800

    commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: () => import('components/recommend/recommend')
+
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: () => import('components/rank/rank')
+
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: () => import('components/search/search')
+
+    },
+    {
+      path: '/singer',
+      name: 'singer',
+      component: () => import('components/singer/singer')
+
+    }
+  ]
+})
