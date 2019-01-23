6c5356542fabe57163caec830784a75fa7a7767a
dinggang <dinggang@lvtudiangdian.com>
2019-01-23 15:06:39 +0800

    轮播组件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot>
+      </slot>
+    </div>
+    <div class="dots">
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots" :key="index"></span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import {addClass} from 'common/js/dom'
+import BScroll from 'better-scroll'
+
+export default {
+  name: 'slider',
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    },
+    speed: {
+      type: Number,
+      default: 400
+    },
+    threshold: {
+      type: Number,
+      default: 0.3
+    }
+  },
+  data () {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this._setSliderWidth()
+      this._initDots()
+      this._initSlider()
+
+      if (this.autoPlay) {
+        this._play()
+      }
+    }, 20)
+
+    window.addEventListener('resize', () => {
+      if (!this.slider) {
+        return
+      }
+      this._setSliderWidth(true)
+      this.slider.refresh()
+    })
+  },
+  // activated () {
+  //   if (this.autoPlay) {
+  //     this._play()
+  //   }
+  // },
+  // deactivated () {
+  //   clearTimeout(this.timer)
+  // },
+  // beforeDestroy () {
+  //   clearTimeout(this.timer)
+  // },
+  methods: {
+    _setSliderWidth (isResize) {
+      this.children = this.$refs.sliderGroup.children
+
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i]
+        addClass(child, 'slider-item')
+
+        child.style.width = sliderWidth + 'px'
+        width += sliderWidth
+      }
+      if (this.loop && !isResize) {
+        width += 2 * sliderWidth
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initSlider () {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true, // 当设置为 true 的时候，可以开启横向滚动。
+        scrollY: false, // 当设置为 true 的时候，可以开启纵向滚动。
+        momentum: false, // 当快速在屏幕上滑动一段距离的时候，会根据滑动的距离和时间计算出动量，并生成滚动动画。设置为 true 则开启动画
+        click: true,
+        snap: {
+          loop: this.loop, // 支持循环轮播
+          threshold: this.threshold, // 表示可滚动到下一个的阈值
+          speed: this.speed // 切换轮播速度
+        }})
+
+      this.slider.on('scrollEnd', () => {
+        // getCurrentPage返回值：{Object} { x: posX, y: posY,pageX: x, pageY: y} 其中，x 和 y 表示偏移的坐标值，pageX 和 pageY 表示横轴方向和纵轴方向的页面数。
+        this.currentPageIndex = this.slider.getCurrentPage().pageX
+
+        if (this.autoPlay) {
+          clearTimeout(this.timer)
+          this._play()
+        }
+      })
+      // this.slider.on('beforeScrollStart', () => {
+      //   if (this.autoPlay) {
+      //     clearTimeout(this.timer)
+      //   }
+      // })
+    },
+    _initDots () {
+      this.dots = new Array(this.children.length)
+    },
+    _play () {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.slider.next() // 滚动到下一个页面
+      }, this.interval)
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+    position relative
+    min-height: 1px
+
+    .slider-group
+      position: relative
+      overflow: hidden
+      white-space: nowrap
+
+      .slider-item
+        float: left
+        box-sizing: border-box
+        overflow: hidden
+        text-align: center
+
+        a
+          display: block
+          width: 100%
+          overflow: hidden
+          text-decoration: none
+
+        img
+          display: block
+          width: 100%
+
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      text-align: center
+      font-size: 0
+
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="recommend" ref="recommend">
     <div>
-      <!--<div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">-->
-      <!--</div>-->
+      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+        <slider>
+          <div v-for="item in recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img class="needsclick" :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul>
@@ -13,10 +20,16 @@
 </template>
 
 <script type="text/ecmascript-6">
+import Slider from 'base/slider/slider'
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
+  data () {
+    return {
+      recommends: []
+    }
+  },
   created () {
     this._getRecommend()
   },
@@ -24,10 +37,13 @@ export default {
     _getRecommend () {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data.slider)
+          this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
