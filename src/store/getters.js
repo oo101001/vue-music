0194925e1fb929e1956c883be653865e7377ae5a
dinggang <476659120@qq.com>
2019-02-04 15:55:04 +0800

    播放列表state

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,17 @@
 export const singer = state => state.singer
+
+export const playing = state => state.playing
+
+export const fullScreen = state => state.fullScreen
+
+export const playList = state => state.playlist
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+export const currenSong = (state) => {
+  return state.playlist[state.currentIndex] || {}
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
+import {playMode} from 'common/js/config'
 const state = {
-  singer: {}
+  singer: {}, // 歌手
+  playing: false, // 播放
+  fullScreen: false, // 全屏
+  playlist: [], // 播放列表
+  sequenceList: [], // 播放序列
+  mode: playMode.sequence, // 播放模式（默认正序）
+  currentIndex: -1 // 当前索引
 }
 
 export default state
